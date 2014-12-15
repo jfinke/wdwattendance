22d3fdf47c979abedd64d8e42be3f4a50c70e153
Jeremy Finke <jeremy.finke@gmail.com>
2014-12-14 19:53:54 -0600

    Working on second plot

--- wdwprocess.R
+++ wdwprocess.R
@@ -14,4 +14,16 @@ g <- ggplot(wdw, aes(x=Year, y = Millions, color = Park))
 
 ggsave(file="total.png") # Saving the plot to total.png
 
+library("dplyr")
+library("reshape")
+
+# Filtering out the older entries where Epcot and DHS was included in MK
+wdw1993<-filter(wdw, Year >= 1993)
+wdw1993$Total<-NULL # Getting rid of the Total column
+
+wdw1993melted<-melt(wdw1993, id = "Year") # Melting the DF
+
+
+
+
 
