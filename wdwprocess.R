e7f7bc7fe277dff9fa5c82683ae3688b2eb4c245
Jeremy Finke <jeremy.finke@gmail.com>
2014-12-14 18:48:29 -0600

    Initial Plot

--- wdwprocess.R
+++ wdwprocess.R
@@ -2,3 +2,6 @@ library("ggplot2")
 
 wdw <- read.delim("wdwattendance.txt", stringsAsFactors=FALSE)
 
+ggplot(wdw, aes(x=Year, y = Millions, color = Park)) +
+  geom_point(aes(y = Total, col = "Total Attendance")) + 
+  geom_point(aes(y = MK, col = "Magic Kingdom"))
